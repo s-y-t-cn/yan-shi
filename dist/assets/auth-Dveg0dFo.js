409629a5ba85c2752335aa78c4df7d51da24ac35
syt <3193299580@qq.com>
2024-07-21 14:46:37 +0800

    1

---
+++ dist/assets/auth-Dveg0dFo.js
@@ -0,0 +1,1 @@
+import{bn as t}from"./index-Bsn8uhFR.js";const o=r=>t({url:"/auth/register",method:"POST",data:r}),s=r=>t({url:"/auth/login",method:"POST",data:r});export{s as l,o as r};
--- dist/assets/index-BWwQj9eg.js
+++
@@ -1,1 +0,0 @@
-const o={url:new URL(import.meta.url),baseUrl:""};o.baseUrl="http://localhost:8080";const t=o;export{t as c};
