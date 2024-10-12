324a69ac6c5e60d8b50292607fd91968a8b6202e
syt <3193299580@qq.com>
2024-10-12 16:37:21 +0800

    1

--- dist/assets/auth-DNF5ZlQF.js
+++
@@ -1,1 +0,0 @@
-import{bv as t}from"./index-g37XHkO7.js";const o=r=>t({url:"/auth/register",method:"POST",data:r}),s=r=>t({url:"/auth/login",method:"POST",data:r});export{s as l,o as r};
---
+++ dist/assets/castArray-BI0f9r0B.js
@@ -0,0 +1,1 @@
+import{ct as t}from"./index-DIMNw_Zj.js";function e(){if(!arguments.length)return[];var r=arguments[0];return t(r)?r:[r]}export{e as c};
