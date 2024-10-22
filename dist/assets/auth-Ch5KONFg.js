44cdc0a5a4c4b93fe7a8c4674f1b29a8dd1b26b6
syt <3193299580@qq.com>
2024-10-22 16:12:46 +0800

    1

--- dist/assets/auth-Ch5KONFg.js
+++
@@ -1,1 +0,0 @@
-import{bS as t}from"./index-BJnXyKVp.js";const o=r=>t({url:"/auth/register",method:"POST",data:r}),s=r=>t({url:"/auth/login",method:"POST",data:r});export{s as l,o as r};
---
+++ dist/assets/auth-iICNMLIm.js
@@ -0,0 +1,1 @@
+import{bS as t}from"./index-CVJ82-Av.js";const o=r=>t({url:"/auth/register",method:"POST",data:r}),s=r=>t({url:"/auth/login",method:"POST",data:r});export{s as l,o as r};
